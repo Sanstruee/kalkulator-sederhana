cbb2c51119326c7d93f99914f8354e42431a2c3c
Sanstruee <sanstruee@gmail.com>
2024-12-09 10:34:15 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,10 @@
-
 import streamlit as st
-
 def main():
+    # Judul
+    st.balloons()
+    st.snow()
     st.title("Kalkulator Sederhana")
     st.write("Aplikasi ini menghitung operasi dasar matematika seperti penjumlahan, pengurangan, perkalian, dan pembagian.")
-    
     # Input angka pertama
     num1 = st.number_input("Masukkan angka pertama:", value=0.0, step=1.0)
 
@@ -21,19 +21,20 @@ def main():
     if st.button("Hitung"):
         if operation == "Penjumlahan":
             result = num1 + num2
-            st.success(f"Hasil Penjumlahan: {result}")
+            st.success(f"Hasil Penjumlahan: {result}",icon=":material/thumb_up:")
         elif operation == "Pengurangan":
             result = num1 - num2
-            st.success(f"Hasil Pengurangan: {result}")
+            st.success(f"Hasil Pengurangan: {result}",icon=":material/thumb_up:")
         elif operation == "Perkalian":
             result = num1 * num2
-            st.success(f"Hasil Perkalian: {result}")
+            st.success(f"Hasil Perkalian: {result}",icon=":material/thumb_up:")
         elif operation == "Pembagian":
             if num2 != 0:
                 result = num1 / num2
-                st.success(f"Hasil Pembagian: {result}")
+                st.success(f"Hasil Pembagian: {result}",icon=":material/thumb_up:")
             else:
-                st.error("Kesalahan: Pembagian dengan nol tidak diperbolehkan.")
+                st.warning('Kesalahan: Pembagian dengan nol tidak diperbolehkan.', icon="⚠️")
     
 if __name__ == "__main__":
     main()
+  
